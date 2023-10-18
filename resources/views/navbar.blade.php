42881cd5513b30d3f079ff5746053464fb7afa43
siwakasen <maderiksi20@gmail.com>
2023-10-18 18:44:57 +0700

    first init

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ngoding Bareng</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        .section-footer{
+            background-color: #333333;
+            height: 100%;
+            color: #c4c4c4;
+        }
+        .container{
+            margin-top: 80px;
+        }
+        .navbar-nav{
+            display: block;
+        }
+        .navbar-item{
+            font-size: 15px;
+            padding: 0 15px;
+            
+        }
+        a img{
+            height: 50px;
+            width: 50px;
+            object-fit: cover;
+            border-radius: 50%;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar fixed-top bg-body-tertiary shadow">
+        <div class="container-fluid text-end">
+            <ul class="py-2 container-fluid navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="#">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Course</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Article</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"></li>
+                <li class="nav-item"><a class="nav-link" href=""><img src="{{asset('images/ryan-gosling.jpg')}}" alt=""></a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fa-sharp fa-solid fa-bars fa-2xl"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure numquam fugit ad! Minus eum repellat commodi
+        corporis autem! Deserunt nihil modi rerum libero eos ad vitae id, quisquam blanditiis sit.
+        Id distinctio unde magni aspernatur veritatis iusto possimus, natus, ratione doloremque voluptatum, quam
+        recusandae architecto ducimus sit magnam reprehenderit sed. Deserunt veritatis error quae provident neque esse
+        ducimus enim harum?
+        Est expedita unde aspernatur sed pariatur, maiores voluptate magnam quisquam vel deleniti consequuntur,
+        blanditiis excepturi temporibus dicta nemo commodi incidunt autem accusantium? Sapiente itaque quaerat omnis
+        doloribus modi eligendi molestiae?
+        Fuga nam a nesciunt est temporibus animi rerum! Officiis enim eveniet assumenda dolore, voluptatum commodi
+        dignissimos esse sint magni tempora repellendus, nihil, excepturi fugit inventore. Quia numquam dolores
+        dignissimos autem.
+        Facilis dolore perferendis ducimus quo aperiam pariatur laboriosam qui corrupti. Eveniet dicta culpa fugit quo
+        velit incidunt. Sed debitis quam eos totam iure. Voluptatum esse exercitationem quibusdam veritatis, itaque
+        iusto!
+        Tenetur perferendis officiis facere nisi voluptatum amet aspernatur totam delectus praesentium, provident velit
+        Voluptatum, ex! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus et ex minus expedita nesciunt consequatur, molestiae repudiandae optio est sint laudantium. Quisquam vel consequatur recusandae, rerum repellendus labore beatae in.
+        Amet, aliquid. Hic sit est quasi necessitatibus quis eos facilis a aliquid! Quas, et saepe? Dolore, harum aspernatur. Doloribus consectetur repellendus tempore explicabo modi nobis veniam dolorum eveniet possimus quae?
+        Nobis numquam modi soluta sint magnam voluptatum saepe obcaecati consequuntur enim at ab tenetur explicabo unde a corporis, perferendis hic, aspernatur maxime id quam nihil voluptas ut illum aperiam. In!
+        Fugiat consequuntur illum quas culpa sint voluptate mollitia, assumenda odio? Ab hic laudantium velit dolore similique odit aliquam placeat officiis porro sunt saepe voluptate, earum veritatis nam quidem necessitatibus quam.
+        Unde consectetur qui distinctio quo suscipit reprehenderit earum perferendis, ipsum nemo nam quasi. Officiis earum deleniti dignissimos beatae eligendi reiciendis, fugit ullam, vitae animi cumque maxime odit, corrupti accusamus eveniet!
+        Totam, reiciendis quod. Quisquam, nesciunt omnis velit odio illum, maxime dicta accusantium eius magnam natus, saepe aliquam! Quod esse architecto enim, optio commodi iure, porro dignissimos velit praesentium, debitis dolore.
+        Labore sunt error odio iste, nam libero perspiciatis quisquam fugit numquam, pariatur animi reiciendis totam doloremque voluptas porro. Facere eaque numquam dolor hic delectus suscipit corrupti quis veniam, autem atque.
+        Sint libero doloribus recusandae in tenetur accusamus, sed culpa eveniet, porro nam deserunt! Omnis nesciunt, provident at consectetur molestias accusamus quos recusandae, tempora repudiandae, harum facere blanditiis pariatur magni neque!
+        Molestias minus nam atque, quas suscipit, itaque est non neque accusamus beatae excepturi natus deleniti doloremque voluptatum aliquam aliquid voluptates a consectetur quos illum maiores tempora ipsa nemo! Quisquam, id?
+        Nobis veritatis hic tenetur sint ratione! Quasi natus corrupti maiores ducimus placeat cupiditate qui eveniet ad, necessitatibus earum numquam provident illum ullam adipisci magni voluptatibus odit consequatur atque, ut deleniti.lorem Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate velit sunt eveniet deleniti blanditiis eos illum totam consectetur. Perferendis, natus. Nulla minima tempora unde exercitationem sed ipsum dicta iusto dolore.
+        Fugiat odio nemo iusto eum eveniet laboriosam ab, deleniti ullam, ipsum adipisci saepe, dolorum incidunt molestias odit praesentium architecto qui porro! Repellendus eaque maxime maiores veritatis voluptas? Praesentium, fugiat quasi.
+        Provident dolorem vero delectus aperiam eius commodi libero nam voluptates minima nihil maxime atque esse unde, consequuntur adipisci architecto id harum tempora magni! Incidunt eum earum nulla sint culpa aliquam.
+        Praesentium eos sint consectetur. Atque nostrum sapiente odio repudiandae ipsa assumenda fugit facere nulla recusandae. Architecto, quos? Iusto soluta doloribus non tempore natus voluptate molestias labore, at alias blanditiis animi!
+        Maiores quaerat, labore dolore earum molestias modi tempora eveniet ad cum dolorum fuga soluta dolores quia dolor eaque, aspernatur et unde nam, minus voluptate odit. Quod incidunt adipisci veniam libero!
+        Nulla, aut rem. Perferendis impedit officiis dolorem omnis qui facilis eius quibusdam, quisquam incidunt autem totam atque a expedita, molestiae exercitationem obcaecati aliquid, voluptate maiores. Consectetur impedit quasi cupiditate recusandae?
+        Debitis, maxime dolores omnis nemo qui quo soluta molestias maiores, sunt excepturi, sint mollitia culpa! Aspernatur aut dicta, dolorum quo sunt molestias iste deserunt, culpa, possimus impedit voluptatum ad atque.
+        Ipsum atque, nihil commodi, dolorum eveniet odit minus ea necessitatibus voluptate quod autem illum quos, beatae ratione ex reprehenderit incidunt. Expedita nisi optio laborum ratione error impedit nam totam at.
+        Unde, harum! Necessitatibus explicabo ipsam asperiores eum distinctio architecto veritatis aut repudiandae enim repellat maxime, nam sit atque est et? Quisquam quibusdam illum, animi labore consequatur voluptatum modi esse delectus!
+        Quasi velit, voluptatibus eligendi excepturi hic quas! Ipsum dicta natus tempore dolores distinctio excepturi unde eveniet ipsam quibusdam! Aspernatur beatae necessitatibus saepe, cum possimus illo tenetur animi alias reiciendis vero?
+        Officiis ipsam sed eos labore saepe quod velit tempora laudantium molestias est? At sit laudantium rerum tempore amet? Sint natus soluta, laboriosam cupiditate ad aliquid nam nostrum facilis odio ea?
+        Autem, soluta amet. Ex, voluptatibus ipsa praesentium repellat modi expedita omnis ipsum ullam, nostrum perspiciatis id? Sunt, expedita praesentium, soluta perspiciatis vero animi at neque, maiores ipsam iste harum repellendus.
+        Eveniet ducimus sapiente, architecto culpa vitae voluptatum repellendus beatae quaerat dicta vel non molestiae sint saepe assumenda, tempore ipsum possimus dolorum sunt dignissimos doloremque commodi accusantium obcaecati, neque dolorem. Cupiditate.
+        Modi, assumenda! Sapiente cupiditate facilis magni cum voluptates fugit nostrum aliquam nobis quae eaque nemo quibusdam, doloremque ipsam veniam excepturi velit corrupti distinctio blanditiis, omnis minus reprehenderit doloribus unde. Iste?
+        Pariatur vel ipsum dolor libero, molestiae ea odit sapiente? Reprehenderit omnis excepturi similique tempora minima in adipisci itaque sunt voluptas? Praesentium repudiandae commodi fugit doloribus vel in labore ut enim.
+        Eveniet exercitationem blanditiis, dignissimos deserunt illum nobis? Voluptas labore culpa sit adipisci natus quibusdam sequi earum dolorem? Labore inventore veritatis at doloremque quidem quisquam harum facere nam consectetur? Rerum, aspernatur?
+        Soluta ad explicabo, commodi nesciunt ut ab quae! Possimus laudantium enim commodi corrupti modi quod rem nulla. Facilis, quaerat magnam alias, voluptatibus cupiditate ducimus eveniet quas vero unde illum corporis?
+        Ratione vitae fuga reprehenderit officia eum enim vel ad magni quod vero sed unde deleniti voluptas itaque quisquam blanditiis sequi atque, eius placeat minus. Quae quasi dignissimos incidunt fugit excepturi!
+        Distinctio, libero. Quo, nisi quod fuga quae aut vitae est inventore placeat tenetur? Ducimus nostrum, vel, ea corrupti minus accusantium voluptate fuga doloribus inventore molestias aut, expedita explicabo nisi officia.
+        Quia totam quis nobis iusto quidem porro asperiores, iste, aut voluptatibus, sit sapiente illum! Voluptatum dignissimos odio aperiam minima corrupti nesciunt obcaecati sit explicabo laborum, excepturi beatae voluptatem, reprehenderit laudantium!
+    </div>
+    <section class="section-footer text-center">
+        <div class="py-5 m-0">
+            <h3>ngoding-bareng</h3>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
