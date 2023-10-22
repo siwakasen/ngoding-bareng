e0563cfc6ca3ad2931c47cf34c086076f776e364
yessalty <yessanugraheni69@gmail.com>
2023-10-22 14:00:47 +0700

    update: login dan manage course

--- resources/views/adminPage/courses/manageCourse.blade.php
+++ resources/views/adminPage/courses/manageCourse.blade.php
@@ -1,4 +1,61 @@
 @extends('navbar')
 @section('content')
-    
+
+<div class="container">
+
+    <div class="row g-3 align-items-center">
+        <div class="col-auto">
+            <label for="inputTitle" class="col-form-label">Title Course</label>
+        </div>
+        <div class="col-auto">
+            <input type="text" id="inputTitle" class="form-control">
+        </div>
+    </div>
+
+    <div class="row g-3 align-items-center">
+        <div class="col-auto">
+            <label for="inputCategory" class="col-form-label">Category</label>
+        </div>
+        <div class="col-auto">
+            <input type="text" id="inputCategory" class="form-control">
+        </div>
+    </div>
+
+    <div class="row g-3 align-items-center">
+        <div class="col-auto">
+            <label for="inputPrice" class="col-form-label">Price</label>
+        </div>
+        <div class="col-auto">
+            <input type="text" id="inputPrice" class="form-control">
+        </div>
+    </div>
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Image Preview</title>
+        <script>
+            function previewImage(event) {
+                var reader = new FileReader();
+                reader.onload = function() {
+                    var output = document.getElementById('preview');
+                    output.src = reader.result;
+                }
+                reader.readAsDataURL(event.target.files[0]);
+            }
+        </script>
+    </head>
+
+    <body>
+        <h2>Pratinjau Gambar</h2>
+        <input type="file" accept="image/*" onchange="previewImage(event)">
+        <br><br>
+        <img id="preview" src="#" alt="Pratinjau Gambar" style="max-width: 300px; max-height: 300px;">
+    </body>
+
+    </html>
+
+
+</div>
 @endsection
--- resources/views/guestPage/loginPage.blade.php
+++ resources/views/guestPage/loginPage.blade.php
@@ -1,4 +1,49 @@
 @extends('navbar')
 @section('content')
-    
+
+<style>
+</style>
+
+
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-body px-4 py-5">
+                            <form class="form" action="{{ url('dashboardAdmin') }}">
+                                @csrf
+                                <div>
+                                    <h2 class="mb-3 text-center"><strong>Login</strong></h2>
+                                </div>
+                                <div class="form-floating mb-2">
+                                    <input type="text" class="form-control" id="floatingInput" name="username" placeholder="Username" required />
+                                    <label for="floatingInput">Username</label>
+                                </div>
+                                <!-- Password -->
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password" required />
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                                <!-- Remember me -->
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" name="remember" checked>
+                                    <label class="form-check-label" for="flexCheckChecked">
+                                        Remember Me
+                                    </label>
+                                </div>
+                                <!-- Submit button -->
+                                <button type="submit" style="width: 100%;" class="btn btn-primary btn-block mb-2 mt-3">
+                                    Login
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
