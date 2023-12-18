fedb28db8158f6920bc46b040fcd5b6dd79bbff2
siwakasen <maderiksi20@gmail.com>
2023-12-19 01:08:45 +0700

    update admin auth

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+
+class Admin extends Authenticatable
 {
     use HasFactory;
     public $timestamps = false;
@@ -15,4 +17,10 @@ class Admin extends Model
         'password_admin',
         'email'
     ];
+
+    protected $guard = 'admin'; 
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 }
